0f974e5eb7a7791eaec9d1d2d85b186684a6f8db
Олег <oleglazorik@mail.ru>
2015-10-25 01:52:11 +0400

    Програма яка розв"язує квадратні рівняння.

---
+++ src/info/teib/lab/oleguar/lab1/Rivnania/rivnynia.java
@@ -0,0 +1,32 @@
+package info.teib.lab.oleguar.lab1.Rivnania;
+
+import java.util.Scanner;
+
+/**
+ * Created by Олег on 24.10.2015.
+ */
+public class rivnynia {
+    public static void main(String[] args) {
+        System.out.println("Введіть значення а,b і с ");
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+        int l = 2;
+        double k = Math.pow(b, l);
+        double d = k - 4 * a * c;
+        double p = Math.sqrt(d);
+        double x1 = (-b + p) / 2 * a;
+        double x2 = (-b - p) / 2 * a;
+        if (d < 0) {
+            System.out.println("D=" + d);
+            System.out.println("Рівняння не має коренів");
+
+        } else
+            System.out.println("D=" + d);
+        System.out.println("x1=" + x1);
+        System.out.println("x2=" + x2);
+
+
+    }
+}
